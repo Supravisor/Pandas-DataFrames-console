7d86af40ecea4f197fd9cd29ed5cdb722b215024
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-03-25 10:27:00 +1300

    add createFromObject function

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ const dataFrame = () => {
      document.editor.textbox.value+= "\n" + variable.value + " = pd.DataFrame({\n    'column 1': [1, 2, 3],\n    'column 2': ['a', 'b', 'c'],\n    'column 3': [7, 8, 9]\n}, columns=['column 1', 'column 2', 'column 3'])";
   }
 }
+
+const createFromObject = () => {
+  if (variable.value === "") {
+    return alert("Please enter a variable name in the 'variable' field, in the 'pandas.DataFrame object' section.");
+  } else if (value.value === "") {
+      return alert("Please enter a value in the 'value' field, in the 'pandas.DataFrame object' section.");
+  } else {
+      document.editor.textbox.value+="\n" + variable.value + " = pd.Series({\n    '': ,\n    '': \n}, name='" + value.value + "')";
+  }
+}
