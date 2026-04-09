5242f545062b1cfab3854051f986e315761a1547
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-04-09 16:13:32 +1200

    add setIndexDataFrame function

--- script.js
+++ script.js
@@ -762,3 +762,17 @@ const resetIndexDataFrame = (arg) => {
       document.editor.textbox.value+="\n" + variable.value + " = " + variable.value + "." + arg;
   }
 }
+
+const setIndexDataFrame = (arg) => {
+  if (variable.value === "") {
+    return alert("Please enter a variable name in the 'variable' field, in the 'pandas.DataFrame object' section.");
+  } else if (Math.abs(Number(variable.value)) >= 0) {
+      return alert("Please do not enter a number in the 'variable' field, in the 'pandas.DataFrame object' section.");
+  } else if (modifyColumn.value === "") {
+      return alert("Please enter a column name in the 'column' field, in the 'Modifying series' section.");
+  } else if (Math.abs(Number(modifyColumn.value)) >= 0) {
+      return alert("Please do not enter a number in the 'column' field, in the 'Modifying series' section.");
+  } else {
+      document.editor.textbox.value+="\n" + variable.value + "." + arg + "('" + modifyColumn.value + "')";
+  }
+}
