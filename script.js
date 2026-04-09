8371d5d2aed75e086293286ebb396ccfa193bee9
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-04-09 15:15:31 +1200

    add modifyDataFrame function

--- script.js
+++ script.js
@@ -651,3 +651,17 @@ const addDataFrame = () => {
       document.editor.textbox.value+="\n" + variable.value + "['" + modifyColumn.value + "'] = " + array.value;
   }
 }
+
+const modifyDataFrame = () => {
+  if (variable.value === "") {
+    return alert("Please enter a variable name in the 'variable' field, in the 'pandas.DataFrame object' section.");
+  } else if (modifyColumn.value === "") {
+      return alert("Please enter a column name in the 'column' field, in the 'Modifying series' section.");
+  } else if (Math.abs(Number(modifyColumn.value)) >= 0) {
+      return alert("Please do not enter a number in the 'array' field, in the 'Modifying series' section.");
+  } else if (modifyValue.value === "") {
+      return alert("Please enter a new value in the 'value' field, in the 'Modifying series' section.");
+  } else {
+      document.editor.textbox.value+="\n" + variable.value + "['" + modifyColumn.value + "'] = '" + modifyValue.value + "'";
+  }
+}
