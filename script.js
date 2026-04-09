7d46c334b1ac856c2f717c619e60f8adedb5f7d9
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-04-09 16:01:01 +1200

    add dropIndexDataFrame function

--- script.js
+++ script.js
@@ -714,3 +714,17 @@ const dropDataFrame = () => {
       document.editor.textbox.value+="\n" + variable.value + ".drop(columns='" + modifyColumn.value + "', inplace=True)";
   }
 }
+
+const dropIndexDataFrame = () => {
+  if (variable.value === "") {
+    return alert("Please enter a variable name in the 'variable' field, in the 'pandas.DataFrame object' section.");
+  } else if (Math.abs(Number(variable.value)) >= 0) {
+      return alert("Please do not enter a number in the 'variable' field, in the 'pandas.DataFrame object' section.");
+  } else if (modifyColumn.value === "") {
+      return alert("Please enter a column name in the 'column' field, in the 'Modifying series' section.");
+  } else if (Math.abs(Number(modifyColumn.value)) >= 0) {
+      return alert("Please do not enter a number in the 'column' field, in the 'Modifying series' section.");
+  } else {
+      document.editor.textbox.value+="\n" + variable.value + ".drop('" + modifyColumn.value + "', inplace=True)";
+  }
+}
