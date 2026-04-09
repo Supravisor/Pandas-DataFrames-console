791f3f026c31bf7722542e5e1a598a543a5aa5ca
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-04-09 12:09:37 +1200

    fix boolean function

--- script.js
+++ script.js
@@ -510,9 +510,9 @@ const boolean = (arg) => {
   } else if (Math.abs(Number(variable.value)) >= 0) {
       return alert("Please do not enter a number in the 'variable' field, in the 'pandas.DataFrame object' section.");
   } else if (valueA.value === "") {
-    return alert("Please enter a value in the 'condition A' field, in the 'Conditional selection (Boolean arrays)' section.");
+    return alert("Please enter a value in the 'value A' field, in the 'Conditional selection (Boolean arrays)' section.");
   } else if (valueB.value === "") {
-      return alert("Please enter a value in the 'condition B' field, in the 'Conditional selection (Boolean arrays)' section.");
+      return alert("Please enter a value in the 'value B' field, in the 'Conditional selection (Boolean arrays)' section.");
   } else {
       if (booleanFilter.value === "") {
         document.editor.textbox.value+="\n" + variable.value + ".loc[" + variable.value + "['" + valueA.value + "'] " + arg + " " + valueB.value + "]";
